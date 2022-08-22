52111ab9c3bf53f847ead991e6b0996be1967188
unknown <taehoon@POSCOICT.AD>
2022-08-22 14:45:26 +0900

    create DecisionTreeIris.pmml

--- app/api.py
+++ app/api.py
@@ -2,29 +2,40 @@ from pathlib import Path
 import numpy as np
 from fastapi import FastAPI, Response
 from joblib import load
+from pypmml import Model
 from .schemas import Wine, Rating, feature_names
 # from .monitoring import instrumentator
 
 ROOT_DIR = Path(__file__).parent.parent
 
 app = FastAPI()
-scaler = load(ROOT_DIR / "artifacts/scaler.joblib")
-model = load(ROOT_DIR / "artifacts/model.joblib")
-
+# scaler = load(ROOT_DIR / "artifacts/scaler.joblib")
+# model = load(ROOT_DIR / "artifacts/model.joblib")
+moodel  = Model.fromFile("DecisionTreeIris.pmml")
 
 @app.get("/")
 def root():
-    return "Wine Quality Ratings"
+    # return "Wine Quality Ratings"
+    return "Iris Species"
+
+# @app.post("/predict", response_model=Rating)
+# def predict(response: Response, sample: Wine):
+#     sample_dict = sample.dict()
+#     features = np.array([sample_dict[f] for f in feature_names]).reshape(1, -1)
+#     features_scaled = scaler.transform(features)
+#     prediction = model.predict(features_scaled)[0]
+#     response.headers["X-model-score"] = str(prediction)
+#     return Rating(quality=prediction)
 
 @app.post("/predict", response_model=Rating)
-def predict(response: Response, sample: Wine):
+def predict(response: Response, sample: Iris):
     sample_dict = sample.dict()
     features = np.array([sample_dict[f] for f in feature_names]).reshape(1, -1)
-    features_scaled = scaler.transform(features)
+    # features_scaled = scaler.transform(features)
+    features_scaled = features
     prediction = model.predict(features_scaled)[0]
     response.headers["X-model-score"] = str(prediction)
-    return Rating(quality=prediction)
-
+    return Rating(Species=prediction)
 
 @app.get("/healthcheck")
 def healthcheck():
--- train.py
+++ train.py
@@ -2,12 +2,16 @@ import logging
 from pathlib import Path
 
 import pandas as pd
-from joblib import dump
-from sklearn import preprocessing
-from sklearn.experimental import enable_hist_gradient_boosting  # noqa
-from sklearn.ensemble import HistGradientBoostingRegressor
-from sklearn.metrics import mean_squared_error
+# from joblib import dump
+# from sklearn import preprocessing
+# from sklearn.experimental import enable_hist_gradient_boosting  # noqa
+# from sklearn.ensemble import HistGradientBoostingRegressor
+# from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn2pmml.pipeline import PMMLPipeline
+from sklearn2pmml import sklearn2pmml
 
 logger = logging.getLogger(__name__)
 
@@ -21,32 +25,50 @@ def prepare_dataset(test_size=0.2, random_seed=1):
                         random_state=random_seed)
     return {"train": train_df, "test": test_df}
 
+# def train():
+#     logger.info("Preparing dataset...")
+#     dataset = prepare_dataset()
+#     train_df = dataset["train"]
+#     test_df = dataset["test"]
+
+#     # separate features from target
+#     y_train = train_df["quality"]
+#     X_train = train_df.drop("quality", axis=1)
+#     y_test = test_df["quality"]
+#     X_test = test_df.drop("quality", axis=1)
+
+#     logger.info("Training model...")
+#     scaler = preprocessing.StandardScaler().fit(X_train)
+#     X_train = scaler.transform(X_train)
+#     X_test = scaler.transform(X_test)
+#     model = HistGradientBoostingRegressor(max_iter=50).fit(X_train, y_train)
+
+#     y_pred = model.predict(X_test)
+#     error = mean_squared_error(y_test, y_pred)
+#     logger.info(f"Test MSE: {error}")
+
+#     logger.info("Saving artifacts...")
+#     Path("artifacts").mkdir(exist_ok=True)
+#     dump(model, "artifacts/model.joblib")
+#     dump(scaler, "artifacts/scaler.joblib")
+
 def train():
     logger.info("Preparing dataset...")
-    dataset = prepare_dataset()
-    train_df = dataset["train"]
-    test_df = dataset["test"]
+
+    iris_df = pd.read_csv("Iris.csv")
 
     # separate features from target
-    y_train = train_df["quality"]
-    X_train = train_df.drop("quality", axis=1)
-    y_test = test_df["quality"]
-    X_test = test_df.drop("quality", axis=1)
+    iris_y = iris_df["Species"]
+    iris_X = iris_df.drop(["Id", "Species"], axis=1)
 
     logger.info("Training model...")
-    scaler = preprocessing.StandardScaler().fit(X_train)
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
-    model = HistGradientBoostingRegressor(max_iter=50).fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-    error = mean_squared_error(y_test, y_pred)
-    logger.info(f"Test MSE: {error}")
+    pipeline = PMMLPipeline([
+        ("classifier", DecisionTreeClassifier())
+    ])
+    pipeline.fit(iris_X, iris_y)
 
     logger.info("Saving artifacts...")
-    Path("artifacts").mkdir(exist_ok=True)
-    dump(model, "artifacts/model.joblib")
-    dump(scaler, "artifacts/scaler.joblib")
+    sklearn2pmml(pipeline, "DecisionTreeIris.pmml", with_repr = True)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
