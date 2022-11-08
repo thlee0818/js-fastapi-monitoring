1fa8f8e4d149e19301db51de74036dbf5080cd07
이태훈(AI기술그룹) <taehoon@poscoict.com>
2022-11-08 14:37:38 +0900

    add prom instru

--- app/api.py
+++ app/api.py
@@ -5,7 +5,7 @@ from joblib import load
 # from pypmml import Model
 from .schemas import Wine, Rating, feature_names
 # from .schemas import Iris, Rating, feature_names
-# from .monitoring import instrumentator
+from .monitoring import instrumentator
 
 ROOT_DIR = Path(__file__).parent.parent
 
@@ -14,6 +14,8 @@ scaler = load(ROOT_DIR / "artifacts/scaler.joblib")
 model = load(ROOT_DIR / "artifacts/model.joblib")
 # model = Model.fromFile("DecisionTreeIris.pmml")
 # model = Model.load("DecisionTreeIris.pmml")
+
+instrumentator.instrument(app).expose(app, include_in_schema=False, should_gzip=True)
 
 @app.get("/")
 def root():
